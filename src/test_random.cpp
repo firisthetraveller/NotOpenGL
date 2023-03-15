496701ae0e7595a22800406716feb40aaa14d642
gluong <16323642+guy-luong@users.noreply.github.com>
2023-03-15 21:07:15 +0100

    Add: Tests on boid generation and neighbour detection

--- src/test_random.cpp
+++ src/test_random.cpp
@@ -1,3 +1,4 @@
+#include "Fish.hpp"
 #include "doctest/doctest.h"
 #include "internal/generate_range.hpp"
 
@@ -9,4 +10,25 @@ TEST_CASE("Random range is good") {
     float tmp = generate_range(-5, 5);
     CHECK((tmp > -5.0 && tmp < 5.0));
   }
+}
+
+TEST_CASE("Boids are generated in the pond") {
+  for (uint i = 0; i < 100; i++) {
+    Fish fish = Fish::generate();
+    CHECK((fish.getData()._center.x > -1 && fish.getData()._center.x < 1.0 &&
+           fish.getData()._center.y > -1 && fish.getData()._center.y < 1.0));
+  }
+}
+
+TEST_CASE("Boids can detect each other") {
+  // With a default range of 0.5
+  Fish fish = Fish::generate();
+  Fish fish2 = Fish({fish.getData()._center.x + 0.3, fish.getData()._center.y},
+                    fish.getData()._radius, fish.getData()._rotation, {0, 0});
+
+  CHECK((fish.isNear(fish2)));
+
+  Fish fish3 = Fish({fish.getData()._center.x + 0.6, fish.getData()._center.y},
+                    fish.getData()._radius, fish.getData()._rotation, {0, 0});
+  CHECK((!fish.isNear(fish3)));
 }
