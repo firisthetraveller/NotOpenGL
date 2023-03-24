cd2ae95e2af70941a6b0daa375b70ac853388b81
gluong <16323642+guy-luong@users.noreply.github.com>
2023-03-24 18:06:40 +0100

    Fix: Data recopy on demand to avoid several existing data

--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,21 @@
 #include "Config.hpp"
 #include "Fish.hpp"
 #include "glm/fwd.hpp"
+#include "imgui.h"
 #include "p6/p6.h"
 #include <cstdlib>
 #include <memory>
 #include <vector>
 #define DOCTEST_CONFIG_IMPLEMENT
 #include "doctest/doctest.h"
+
+static std::vector<FishData> getDataFromFish(std::vector<Fish> &fish) {
+  std::vector<FishData> data(fish.size());
+  for (Fish &f : fish) {
+    data.emplace_back(f.getData());
+  }
+  return data;
+}
 
 int main(int argc, char *argv[]) {
   { // Run the tests
@@ -30,12 +39,10 @@ int main(int argc, char *argv[]) {
   Config::getInstance().ASPECT_RATIO = ctx.aspect_ratio();
 
   std::vector<Fish> fishs;
-  std::vector<FishData> fishData;
 
-  for (uint i = 0; i < 100; i++) {
+  for (unsigned int i = 0; i < 50; i++) {
     fishs.emplace_back(Fish::generate());
     // fishs[i].addBehavior(BehaviorFactory::teleport());
-    fishData.emplace_back(fishs[i].getData());
   }
 
   // ImGui demo
@@ -43,31 +50,36 @@ int main(int argc, char *argv[]) {
 
   ctx.imgui = [&]() {
     ImGui::Begin("Parameters");
+    ImGui::TextColored(ImVec4(1, 1, 0, 1), "Boid parameters");
     ImGui::SliderFloat("Visual range", &Config::getInstance().VISUAL_RANGE, 0.f,
-                       1.f);
+                       0.2f);
     ImGui::SliderFloat("Vital space", &Config::getInstance().MIN_DISTANCE, 0.f,
-                       0.3f);
+                       0.1f);
     ImGui::SliderFloat("Speed limit", &Config::getInstance().SPEED_LIMIT, 0.f,
-                       0.3f);
+                       0.01f);
+    ImGui::TextColored(ImVec4(1, 1, 0, 1), "Behavior parameters");
     ImGui::SliderFloat("Separation", &Config::getInstance().SEPARATION_FACTOR,
-                       0.f, 1.f);
+                       0.f, 0.01f);
     ImGui::SliderFloat("Alignment", &Config::getInstance().ALIGNMENT_FACTOR,
-                       0.f, 1.f);
+                       0.f, 0.1f);
     ImGui::SliderFloat("Cohesion", &Config::getInstance().COHESION_FACTOR, 0.f,
-                       1.f);
+                       0.01f);
+    ImGui::TextColored(ImVec4(1, 1, 0, 1), "Wall avoidance parameters");
     ImGui::SliderFloat("Wall margin", &Config::getInstance().WALL_MARGIN, 0.f,
-                       0.5f);
+                       0.2f);
     ImGui::SliderFloat("Wall turn", &Config::getInstance().WALL_TURN_FACTOR,
-                       0.f, 0.5f);
+                       0.f, 0.05f);
     ImGui::End();
     // Show the official ImGui demo window
     // It is very useful to discover all the widgets available in ImGui
-    // ImGui::ShowDemoWindow();
+    ImGui::ShowDemoWindow();
   };
 
   // Declare your infinite update loop.
   ctx.update = [&]() {
     ctx.background(p6::NamedColor::Blue);
+
+    std::vector<FishData> fishData = getDataFromFish(fishs);
 
     // Comparison square
     // ctx.square(p6::Center{0, 0}, p6::Radius{1});
